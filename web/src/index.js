697d4ba27a05d3bd4bbc42061c4ee8af44ef926e
黄建峰 <645381995@qq.com>
2016-08-18 16:15:54 +0800

    在发布下

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,53 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render} from 'react-dom'
 import App from './components/Main';
 import Store from './stores/Store';
+import {Router, Route, Link, browserHistory} from 'react-router'
 
 const store = new Store();
 
-ReactDOM.render(<App store={store}/>, document.getElementById('app'));
+
+const Users = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        <div className="master">
+
+        </div>
+        <div className="detail">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+});
+
+const User = React.createClass({
+  componentDidMount() {
+
+  },
+  render() {
+    return (
+      <div>
+        <h2>111</h2>
+      </div>
+    )
+  }
+});
+
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Users}>
+      <Route path="users" component={Users}>
+        <Route path="/user/:userId" component={User}/>
+      </Route>
+    </Route>
+  </Router>
+), document.getElementById('app'));
+
+
+{/*ReactDOM.render(<App store={store}/>, document.getElementById('app'));*/}
