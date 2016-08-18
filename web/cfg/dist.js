a25d78fc0e745c8a830785ba2182b69a974d5dca
黄建峰 <645381995@qq.com>
2016-08-18 10:03:58 +0800

    dist

--- web/cfg/dist.js
+++ web/cfg/dist.js
@@ -12,7 +12,8 @@ let BowerWebpackPlugin = require('bower-webpack-plugin');
 let config = Object.assign({}, baseConfig, {
   entry: path.join(__dirname, '../src/index'),
   cache: false,
-  devtool: 'sourcemap',
+  // devtool: 'sourcemap',
+  devtool: false,
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
