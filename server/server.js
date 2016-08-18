5f4673f427cbf47d9df9eeca4059e7104c13b8c6
黄建峰 <645381995@qq.com>
2016-08-18 10:37:21 +0800

    dist

--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ const express = require('express');
 module.exports = {
     app: function () {
         const app = express();
-        const indexPath = path.join(__dirname , 'index.html');
-        const a=__dirname;
+        const indexPath = path.join(__dirname, 'index.html');
         // const publicPath = express.static(path.join(___dirname, '../web/dist'));
         const publicPath = express.static(path.join(__dirname, '../web/src'));
         app.use('/publish', publicPath);
