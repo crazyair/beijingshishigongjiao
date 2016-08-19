b9ea7f92e70969ac074c24ca138376bbce744ff4
黄建峰 <645381995@qq.com>
2016-08-19 10:16:08 +0800

    and-mobie

--- web/src/components/List.js
+++ web/src/components/List.js
@@ -1,8 +1,13 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
-
+// import 'amazeui-touch/dist/amazeui.touch.min.css';
 import React from 'react';
 import {observer} from 'mobx-react';
+// import {
+//   Button
+// } from 'amazeui-touch';
+import 'antd-mobile/lib/button/style';
+import Button from 'antd-mobile/lib/button';
 let yeomanImage = require('../images/yeoman.png');
 @observer
 class List extends React.Component {
@@ -29,8 +34,9 @@ class List extends React.Component {
         <img src={yeomanImage} alt="Yeoman Generator"/>
         <h2>{this.store.name}</h2>
         <button onClick={this.click.bind(this) }>点我ck</button>
-        <button onClick={this.click2.bind(this) }>点我获取线路 </button>
+        <button onClick={this.click2.bind(this) }>点我获取线路</button>
         <h4>{this.store.numClicks}</h4>
+        <Button>Hello World</Button>
       </div>
     );
   }
